eec912bd697a961853297bb2dececf405489b3a9
stoneport <stoneport@users.noreply.github.com>
2024-11-28 07:50:38 +0000

    Libbox Release 1.10.3

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/EbrahimTahernejad/sing-box-lib/releases/download/1.9.0/Libbox.xcframework.zip",
-      checksum: "4655377463ffc951184f801e67c129e4da97dabcd8777c8d55683ad041715134"
+      url: "https://github.com/EbrahimTahernejad/sing-box-lib/releases/download/1.10.3/Libbox.xcframework.zip",
+      checksum: "a88ad5a4c0e259ea18b09b762ba2c95025e0a95f95661be2385e7501c4b81873"
     )
   ]
 )
